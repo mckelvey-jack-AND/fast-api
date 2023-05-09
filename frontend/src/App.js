e592e2652c11cd7b354c0dcd986d21f63002be18
Musa <79163385+musayuksel@users.noreply.github.com>
2023-05-09 15:52:31 +0100

    Add prettier tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <h1>Welcome to the Quiz App</h1>;
+  return (
+    <h1>
+      Welcome to the Quiz App
+      <p>test</p>
+    </h1>
+  );
 }
 
 export default App;
