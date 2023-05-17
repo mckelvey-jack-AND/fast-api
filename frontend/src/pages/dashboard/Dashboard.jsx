77f8d2336fac4972644f74be48392bb1802600d1
zainabnoori <zainab.noori@and.digital>
2023-05-17 13:50:47 +0100

    Add the individual and squad tabs

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./dashboard.module.css";
+import Individual from "./Individual";
+import Squad from "./Squad";
 
 const Dashboard = () => {
-  return <div className={styles.dashboardContainer}>Dashboard</div>;
+  const [active, setActive]= useState('individual');
+
+  return (
+    <div className={styles.tabs}>
+    <ul className={styles.nav}>
+      <li onClick={()=>{setActive('individual')}}>Individual</li>
+      <li onClick={()=>{setActive('Squad')}}>Squad</li>
+    </ul>
+    <div className={styles.outlet}>
+      {(active === 'individual') ? <Individual/> : <Squad/> }
+    </div>
+  </div>
+  );
 };
 
 export default Dashboard;
---
+++ frontend/src/pages/dashboard/Squad.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Squad = () => {
+  return (
+    <div >
+      <p>Squad dashboard content</p>
+
+    </div>
+  );
+};
+export default Squad;
