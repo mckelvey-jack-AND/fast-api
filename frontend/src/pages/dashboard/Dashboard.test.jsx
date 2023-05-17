082f439853686e7bdbcbd339ccd42a7b49358f3c
Musa Yuksel <musa.yuksel@and.digital>
2023-05-17 16:22:12 +0100

    Refactor file structure and fix typos

--- frontend/src/pages/dashboard/Dashboard.test.jsx
+++ frontend/src/pages/dashboard/Dashboard.test.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import Dashboard from "./Dashboard";
-describe("Dashboard", () => {
-  it("renders correctly", () => {
+
+describe("Dashboard component", () => {
+  it("should render Individual dashboard content as a default", () => {
     render(<Dashboard />);
-    expect(true).toBe(true);
+
+    const individualDashboardContent = screen.getByText(
+      "Individual dashboard content"
+    );
+
+    expect(individualDashboardContent).toBeVisible();
+  });
+
+  it("should render Squad content after clicking on Squad tab", () => {
+    render(<Dashboard />);
+
+    const individualTab = screen.getByText("Squad");
+
+    // Check that 'Individual dashboard content' text is not visible initially
+    expect(screen.queryByText("Squad dashboard content")).toBeNull();
+
+    // Click on individual tab
+    fireEvent.click(individualTab);
+
+    expect(screen.queryByText("Squad dashboard content")).toBeVisible();
   });
 });
