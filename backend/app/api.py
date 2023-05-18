6ca6ce47aa40e9b3f3954fbdd0750a7ef09a920d
zainabnoori <zainab.noori@and.digital>
2023-05-18 13:11:33 +0100

    Add the leaderboard query and create the leaderboard endpoint

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from queries.leaderboard import get_leaderboard_data
 
 app = FastAPI()
 
@@ -15,7 +15,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# query=f'''
+# SELECT user_id,users.first_name,users.last_name,users.squad, SUM(answers.is_correct) AS "total score" FROM user_answers
+# LEFT JOIN users ON users.ID = user_answers.user_id
+# LEFT JOIN answers ON answers.id = user_answers.answer_id
+# WHERE answers.is_correct =1
+# GROUP BY user_id
+# ORDER BY SUM(answers.is_correct) DESC
+# LIMIT 10;
+# '''
+
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     
     return {"message": "Hello world."}
+
+@app.get("/leaderboard")
+def get_data():
+    data = get_leaderboard_data()
+    return {"data": data}
