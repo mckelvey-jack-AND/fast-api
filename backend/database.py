9aa9d68bc85ff539b86015747ba327533b752d97
Jack McKelvey <jack.mckelvey@and.digital>
2023-05-18 12:20:32 +0100

    add new db connection structure

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv('../.env')
+
+# Establish a connection to the Google Cloud MySQL database
+db_host = os.environ.get('DB_HOST')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+
+
+connection = pymysql.connect(
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name,
+    cursorclass=pymysql.cursors.DictCursor
+)
