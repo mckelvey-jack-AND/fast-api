085a7995698fcb26dfb7e5f61f6ba617a1c14771
Musa Yuksel <musa.yuksel@and.digital>
2023-05-18 17:28:46 +0100

    Add error handling

--- frontend/src/components/graphs/GroupedBarChart.jsx
+++ frontend/src/components/graphs/GroupedBarChart.jsx
@@ -1,14 +1,18 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-console */
 import React, { useEffect } from "react";
 import useFetch from "../../hooks/useFetch";
-
+import { useNavigate } from "react-router-dom";
 const GroupedBarChart = () => {
   const { data: graphData, error, loading } = useFetch("/correct-answers");
+
+  const navigate = useNavigate();
+
   useEffect(() => {
-    console.log(graphData);
-  }, []);
-  return <div></div>;
+    error && navigate("/error");
+  }, [error, navigate]);
+
+  // eslint-disable-next-line no-console
+  console.log({ graphData });
+  return <div>{loading && <div>Loading...</div>}</div>;
 };
 
 export default GroupedBarChart;
