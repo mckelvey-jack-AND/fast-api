a6ad081483c28ecac46390c1703e25941de552b6
zainabnoori <zainab.noori@and.digital>
2023-05-18 13:34:53 +0100

    fetch the leaderboard data in the dashboard

--- frontend/src/pages/dashboard/individual/Individual.jsx
+++ frontend/src/pages/dashboard/individual/Individual.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
+
 
 const Individual = () => {
+
+  const leaderboardData = () =>{
+    fetch('/leaderboard')
+  .then(response => response.data)
+  .then(response => {
+    return response
+  })
+}
+
+useEffect(() => {
+  leaderboardData();
+}, []);
+
+
   return (
     <div>
       <p>Individual dashboard content</p>
