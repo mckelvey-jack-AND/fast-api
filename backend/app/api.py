53a1fc930afe1c1fd6f1f9830a392bb0f70a19e5
zainabnoori <zainab.noori@and.digital>
2023-05-18 19:55:32 +0100

    remove comments

--- backend/app/api.py
+++ backend/app/api.py
@@ -15,16 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# query=f'''
-# SELECT user_id,users.first_name,users.last_name,users.squad, SUM(answers.is_correct) AS "total score" FROM user_answers
-# LEFT JOIN users ON users.ID = user_answers.user_id
-# LEFT JOIN answers ON answers.id = user_answers.answer_id
-# WHERE answers.is_correct =1
-# GROUP BY user_id
-# ORDER BY SUM(answers.is_correct) DESC
-# LIMIT 10;
-# '''
-
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     
