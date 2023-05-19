207522251895b25e5475c65324454286a12a2527
Musa Yuksel <musa.yuksel@and.digital>
2023-05-19 12:30:09 +0100

    Update variable names

--- backend/queries/test_correct_answer_fotmat.py
+++ backend/queries/test_correct_answer_fotmat.py
@@ -1,32 +1,32 @@
 from correct_answer_fotmat import group_by_rounds, get_quiz_categories, get_rounds
 answers_from_db = [
     {
-        "rounds": "1",
+        "rounds": "quiz_1",
         "difficulty": "Easy",
         "total_correct": "1",
     },
     {
-        "rounds": "1",
+        "rounds": "quiz_1",
         "difficulty": "Medium",
         "total_correct": "2",
     },
     {
-        "rounds": "1",
+        "rounds": "quiz_1",
         "difficulty": "Hard",
         "total_correct": "3",
     },
     {
-        "rounds": "2",
+        "rounds": "quiz_2",
         "difficulty": "Easy",
         "total_correct": "4",
     },
     {
-        "rounds": "2",
+        "rounds": "quiz_2",
         "difficulty": "Medium",
         "total_correct": "5",
     },
     {
-        "rounds": "2",
+        "rounds": "quiz_2",
         "difficulty": "Hard",
         "total_correct": "6",
     },
@@ -34,12 +34,12 @@ answers_from_db = [
 
 
 quiz_categories = {
-    "1": {
+    "quiz_1": {
         "easy": 1,
         "medium": 2,
         "hard": 3,
     },
-    "2": {
+    "quiz_2": {
         "easy": 4,
         "medium": 5,
         "hard": 6,
@@ -48,13 +48,13 @@ quiz_categories = {
 
 answers_for_graph = [
     {
-        "name": "1",
+        "name": "quiz_1",
         "easy": 1,
         "medium": 2,
         "hard": 3,
     },
     {
-        "name": "2",
+        "name": "quiz_2",
         "easy": 4,
         "medium": 5,
         "hard": 6,
