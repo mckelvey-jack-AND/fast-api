352d8365cebca13aa2bedf6c60c7117a1b2224e9
zainabnoori <zainab.noori@and.digital>
2023-05-22 20:09:15 +0100

    Add the quiz logic

--- frontend/src/pages/quiz/Quiz.jsx
+++ frontend/src/pages/quiz/Quiz.jsx
@@ -1,7 +1,79 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./quiz.module.css";
 
 const Quiz = () => {
-  return <div style={{ height: "500px", backgroundColor: "yellow" }}>Quiz</div>;
+  //const [quizData, setQuizData] = useState(null);
+  const [question, setQuestion] = useState("");
+  const [answers, setAnswers] = useState([]);
+  const [endQuiz, setEndQuiz] = useState(false);
+  const [correctAnswer, setCorrectAnswer] = useState("");
+  const [totalCorrectAnswer, setTotalCorrectAnswer] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const handleNextClick = (answer) => {
+    if (currentQuestion === 40) {
+      setEndQuiz(true);
+    }
+    if (answer === correctAnswer) {
+      setTotalCorrectAnswer(totalCorrectAnswer + 1);
+    }
+    setCurrentQuestion(currentQuestion + 4);
+    questionDB();
+  };
+
+  
+  const questionDB = async () => {
+    try {
+      let res = await axios.get('/quiz');
+      let result = res.data;
+     
+      setQuestion(result.data[currentQuestion].question_text);
+      setAnswers(
+        [result.data[currentQuestion].answer_text,
+        result.data[currentQuestion+1].answer_text,
+        result.data[currentQuestion+2].answer_text,
+        result.data[currentQuestion+3].answer_text]);
+        if (result.data[currentQuestion].isCorrect === 1) {
+          setCorrectAnswer(result.data[currentQuestion]);
+        } else if (result.data[currentQuestion+1].answer_text){
+          setCorrectAnswer(result.data[currentQuestion+1].answer_text);
+        } else if (result.data[currentQuestion+2].answer_text){
+          setCorrectAnswer(result.data[currentQuestion+2].answer_text);
+        } else if (result.data[currentQuestion+3].answer_text){
+          setCorrectAnswer(result.data[currentQuestion+3].answer_text);
+        }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+   
+  useEffect( () => {
+    questionDB()
+  }, []);
+
+  return (
+    <div>
+        {endQuiz ? (
+          <>
+            <div className={styles.question}> Quiz End!</div>
+            <div>{totalCorrectAnswer - 1} correct answers</div>
+          </>
+        ) : (
+          <>
+            <div className={styles.question}>{question}</div>
+            <div className={styles.answer}>
+                {answers.map((answer, index) => {
+                  return <button key={index} className={styles.button_answer} onClick={() => handleNextClick(answer)}>{answer}</button>
+                })}
+              </div>
+          </>
+        )}
+
+    </div>
+
+  );
 };
 
 export default Quiz;
