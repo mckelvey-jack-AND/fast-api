2ed7c458a6394140f05c79e7d82fb525aafc254c
Jack McKelvey <jack.mckelvey@and.digital>
2023-05-22 13:26:02 +0100

    add individual test

--- backend/app/test_api.py
+++ backend/app/test_api.py
@@ -9,8 +9,7 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world."}
-
-
+    
 def test_leaderboard():
-    response = client.get("/leaderboard")
+    response = client.get("/leaderboard?type=individual")
     assert response.status_code == 200
