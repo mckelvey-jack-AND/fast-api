03be3fafb1d0cfd882816d13a80f5a56ed007351
Musa Yuksel <musa.yuksel@and.digital>
2023-05-22 11:26:07 +0100

    Fix tests

--- frontend/src/pages/dashboard/Dashboard.test.jsx
+++ frontend/src/pages/dashboard/Dashboard.test.jsx
@@ -1,31 +1,37 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
-// import Dashboard from "./Dashboard";
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 describe("Dashboard component", () => {
-  // it("should render Individual dashboard content as a default", () => {
-  //   render(<Dashboard />);
+  it("should render Individual dashboard content as a default", () => {
+    render(<Dashboard />);
 
-  //   const individualDashboardContent = screen.getByText(
-  //     "Individual dashboard content"
-  //   );
+    const individualDashboardContent = screen.getByText(
+      "Individual dashboard content"
+    );
 
-  //   expect(individualDashboardContent).toBeVisible();
-  // });
+    expect(individualDashboardContent).toBeVisible();
+  });
 
-  // it("should render Squad content after clicking on Squad tab", () => {
-  //   render(<Dashboard />);
+  it("should render Squad content after clicking on Squad tab", () => {
+    render(<Dashboard />);
 
-  //   const individualTab = screen.getByText("Squad");
+    const individualTab = screen.getByText("Squad");
 
-  //   // Check that 'Individual dashboard content' text is not visible initially
-  //   expect(screen.queryByText("Squad dashboard content")).toBeNull();
+    // Check that 'Individual dashboard content' text is not visible initially
+    expect(screen.queryByText("Squad dashboard content")).toBeNull();
 
-  //   // Click on individual tab
-  //   fireEvent.click(individualTab);
+    // Click on individual tab
+    fireEvent.click(individualTab);
 
-  //   expect(screen.queryByText("Squad dashboard content")).toBeVisible();
-  // });
+    expect(screen.queryByText("Squad dashboard content")).toBeVisible();
+  });
   it("Mock test", () => {
     expect(true).toBe(true);
   });
--- frontend/src/pages/dashboard/individual/Individual.test.jsx
+++ frontend/src/pages/dashboard/individual/Individual.test.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Individual from "./Individual";
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 describe("Individual component", () => {
   it("should render Individual dashboard content", () => {
