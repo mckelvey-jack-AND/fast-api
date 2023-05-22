b442067d90c18b250c67c06a765d677de48a2436
zainabnoori <zainab.noori@and.digital>
2023-05-22 12:04:04 +0100

    Changed the import bath

--- backend/queries/test_leaderboard.py
+++ backend/queries/test_leaderboard.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from ..main import app
+from backend.main import app
 
-app = FastAPI()
+# app = FastAPI()
 
 
-@app.get("/leaderboard")
-async def read_main():
-    return {"data":[{"squad":"squad_1","total score":124},{"squad":"squad_3","total score":121},{"squad":"squad_2","total score":103}]}
+# @app.get("/leaderboard")
+# async def read_main():
+#     return {"data":[{"squad":"squad_1","total score":124},{"squad":"squad_3","total score":121},{"squad":"squad_2","total score":103}]}
 
 
 client = TestClient(app)
 
-
-def test_read_main():
+def test_leaderboard():
     response = client.get("/leaderboard")
     assert response.status_code == 200
