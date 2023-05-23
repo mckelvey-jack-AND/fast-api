2515b8acd622cb10629d76c0307017dbd94a162f
Musa Yuksel <musa.yuksel@and.digital>
2023-05-23 10:46:07 +0100

    Open commented out tests

--- backend/app/test_api.py
+++ backend/app/test_api.py
@@ -1,11 +1,11 @@
-# from fastapi.testclient import TestClient
+from fastapi.testclient import TestClient
 
-# from .api import app
+from .api import app
 
-# client = TestClient(app)
+client = TestClient(app)
 
 
-# def test_read_main():
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert response.json() == {"message": "Hello world."}
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello world."}
