ef60b51e011fa119ed6e6139b6581fd34beadd38
Musa Yuksel <musa.yuksel@and.digital>
2023-05-23 10:37:12 +0100

    Delete unused tests

--- backend/app/test_api.py
+++ backend/app/test_api.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-from unittest.mock import patch
 
 from .api import app
 
@@ -9,12 +8,3 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world."}
-    
-# def test_read_correct_answers():
-#     response = client.get("/correct-answers")
-#     assert response.status_code == 200
-    
-    
-# def test_leaderboard():
-#     response = client.get("/leaderboard?type=individual")
-#     assert response.status_code == 200
