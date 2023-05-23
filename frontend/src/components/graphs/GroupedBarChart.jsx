54a473db174dcedca57570f668389c465704ee8e
Jack McKelvey <jack.mckelvey@and.digital>
2023-05-23 13:40:02 +0100

    make graoh responsive

--- frontend/src/components/graphs/GroupedBarChart.jsx
+++ frontend/src/components/graphs/GroupedBarChart.jsx
@@ -9,6 +9,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 const GroupedBarChart = () => {
   const { data: graphData, error, loading } = useFetch("/correct-answers");
@@ -21,17 +22,20 @@ const GroupedBarChart = () => {
   return (
     <div>
       {loading && <div>Loading...</div>}
-
-      <BarChart width={730} height={250} data={graphData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="easy" fill="#acdf87" />
-        <Bar dataKey="medium" fill="#ffc500" />
-        <Bar dataKey="hard" fill="#e61717" />
-      </BarChart>
+      <div style={{ height: "400px", width: "100%" }}>
+        <ResponsiveContainer width={"100%"} height="100%">
+          <BarChart data={graphData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis width={25} />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="easy" fill="#acdf87" />
+            <Bar dataKey="medium" fill="#ffc500" />
+            <Bar dataKey="hard" fill="#e61717" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
