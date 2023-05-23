97c3390813a1b0426851b7eaf6353cac2ca67e49
Musa Yuksel <musa.yuksel@and.digital>
2023-05-23 10:23:18 +0100

    Prettier lint

--- backend/app/api.py
+++ backend/app/api.py
@@ -31,11 +31,11 @@ async def read_correct_answers() -> dict:
 
     return {"data": quiz_rounds}
 
+
 @app.get("/leaderboard")
 def get_data(type: str):
-
-    if type != "squad" and type != 'individual':
+    if type != "squad" and type != "individual":
         raise HTTPException(status_code=404, detail="Type must be individual or squad")
-    
+
     data = get_leaderboard_data(type)
     return {"data": data}
