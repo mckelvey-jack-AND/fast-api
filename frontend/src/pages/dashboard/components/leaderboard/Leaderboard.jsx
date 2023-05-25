6ff57db5cc4a7f64b62027287673e10f0c5d2c76
Musa Yuksel <musa.yuksel@and.digital>
2023-05-25 13:25:59 +0100

    Add Leaderboard style

--- frontend/src/pages/dashboard/components/leaderboard/Leaderboard.jsx
+++ frontend/src/pages/dashboard/components/leaderboard/Leaderboard.jsx
@@ -1,18 +1,31 @@
 import React from "react";
 import styles from "./leaderboard.module.css";
+import { getOrdinal } from "../../../../helpers/getOrdinal";
 
 const Leaderboard = ({ userData, type }) => {
   return (
     <div className={styles.leaderboard}>
+      <h3 className={styles.leaderboard_header}>
+        {type} Leaderboard this week
+      </h3>
       <ol className={styles.orderList}>
-        {userData.map((user) => {
+        <li className={styles.list_header}>
+          <div>Place</div>
+          <div>{type === "individual" ? "Name" : "Squad"}</div>
+          <div>Score</div>
+        </li>
+        {userData.map((user, index) => {
           const name =
             type === "squad"
               ? user.squad
               : `${user.first_name} ${user.last_name}`;
 
           return (
-            <li key={name} className={styles.list}>
+            <li key={name} className={styles.list_item}>
+              <div className={styles.place}>
+                {index + 1}
+                {getOrdinal(index + 1)}
+              </div>
               <div className={styles.name}>{name}</div>
               <div className={styles.score}>{user["total score"]}</div>
             </li>
