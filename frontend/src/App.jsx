d9eecff7868483f58bd7479815658e7c5f67f80b
Musa Yuksel <musa.yuksel@and.digital>
2023-05-26 12:39:17 +0100

    Add current user provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import { UserContext } from "./hooks/UserContext";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("");
+
   return (
     <>
-      <Header />
-      <main>
-        {/* Dynamic main content */}
-        <Outlet />
-      </main>
-      <Footer />
+      <UserContext.Provider value={{ currentUser, setCurrentUser }}>
+        <Header />
+        <main>
+          {/* Dynamic main content */}
+          <Outlet />
+        </main>
+        <Footer />
+      </UserContext.Provider>
     </>
   );
 }
