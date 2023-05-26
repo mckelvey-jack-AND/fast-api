3694b5ff10e0701eaea9425539be7333aa63493a
Musa Yuksel <musa.yuksel@and.digital>
2023-05-26 09:39:16 +0100

    Update strick equality

--- frontend/src/helpers/getOrdinal.js
+++ frontend/src/helpers/getOrdinal.js
@@ -1,11 +1,11 @@
 export const getOrdinal = (number) => {
   let ordinal = "th";
 
-  if (number % 10 === 1 && number % 100 != 11) {
+  if (number % 10 === 1 && number % 100 !== 11) {
     ordinal = "st";
-  } else if (number % 10 === 2 && number % 100 != 12) {
+  } else if (number % 10 === 2 && number % 100 !== 12) {
     ordinal = "nd";
-  } else if (number % 10 === 3 && number % 100 != 13) {
+  } else if (number % 10 === 3 && number % 100 !== 13) {
     ordinal = "rd";
   }
 
